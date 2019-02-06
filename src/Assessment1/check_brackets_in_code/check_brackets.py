943494206db93a63b14ddb65dc11b05a0287e6b4
fangc <saij84@gmail.com>
2019-02-06 19:41:48 +1100

    assessment 1 part 1

--- src/Assessment1/check_brackets_in_code/check_brackets.py
+++ src/Assessment1/check_brackets_in_code/check_brackets.py
@@ -15,17 +15,24 @@ def are_matching(left, right):
 def find_mismatch(text):
     if len(text.strip()) > 1:
         opening_brackets_stack = []
+        closing_brackets_stack = []
         for i, next in enumerate(text):
             if next in "([{":
                 # Process opening bracket, write your code here
-                opening_brackets_stack.append(next)
+                opening_brackets_stack.append((next, i))
 
             if next in ")]}":
                 # Process closing bracket, write your code here
-                if are_matching(opening_brackets_stack[-1], next) == False:
-                    return i+1
+                closing_brackets_stack.append((next, i))
+
+        if len(opening_brackets_stack) == len(closing_brackets_stack):
+            for closing in closing_brackets_stack:
+                if are_matching(opening_brackets_stack[-1][0], closing[0]) == False:
+                    return closing[1]+1
                 else:
                     opening_brackets_stack.pop(-1)
+
+
         return "Success"
     else:
         return 1
@@ -36,7 +43,7 @@ def main():
     file_num = 2
 
     with open(files + "\{:02}".format(file_num)) as inFile:
-        text = inFile.read()
+        text = "[](()}" #inFile.read()
         mismatch = find_mismatch(text)
         print(mismatch)
 
