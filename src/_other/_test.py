8fd1c22921160c29dc604ac1646032ed83251097
fangc <saij84@gmail.com>
2019-02-09 21:41:58 +1100

    assessment 1 part2

--- src/_other/_test.py
+++ src/_other/_test.py
@@ -1,18 +1,52 @@
-def is_matched(expression):
-  opening = tuple('({[')
-  closing = tuple(')}]')
-  mapping = dict(zip(opening, closing))
-  queue = []
+# python3
 
-  for letter in expression:
-    if letter in opening:
-      queue.append(mapping[letter])
-    elif letter in closing:
-      if not queue or letter != queue.pop():
-        return False
-  return not queue
+import sys
+import threading
+
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 25)  # new thread will get stack of such size
 
 
+class TreeHeight:
+    """Computes height of a given tree.
+    Height of a (rooted) tree is the maximum depth of a node, or the maximum
+    distance from a leaf to the root.
+    """
+
+    def __init__(self):
+        self.n = 0
+        self.parent = []
+        self.cache = []
+
+    def read(self):
+        """Reads data from standard input."""
+        self.n = int(sys.stdin.readline())
+        self.parent = list(map(int, sys.stdin.readline().split()))
+        self.cache = [0] * self.n
+
+    def path_len(self, node_id):
+        """Returns path length from given node to the root."""
+        parent = self.parent[node_id]
+
+        if parent == -1:
+            return 1
+
+        if self.cache[node_id]:
+            return self.cache[node_id]
+
+        self.cache[node_id] = 1 + self.path_len(self.parent[node_id])
+        print("self.cache[node_id]:", self.cache[node_id])
+        return self.cache[node_id]
+
+    def compute_height(self):
+        """Computes the tree height."""
+        return max([self.path_len(i) for i in range(self.n)])
 
 
-print(is_matched('[](()'))
+def main():
+    tree = TreeHeight()
+    tree.read()
+    print(tree.compute_height())
+
+
+threading.Thread(target=main).start()
